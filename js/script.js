a358eb69df2d11903c489cba3861b4679b604f60
Isaque Bersalim <isaquebersalim@gmail.com>
2025-06-10 21:14:48 -0300

    Coleta dados do formulário e estrutura objeto com as informações da necessidade

--- js/script.js
+++ js/script.js
@@ -4,5 +4,15 @@ if (form) {
   form.addEventListener('submit', function(e) {
     e.preventDefault();
     
+    const necessidade = {
+      instituicao: document.getElementById('instituicao').value,
+      tipoAjuda: document.getElementById('tipo-ajuda').value,
+      titulo: document.getElementById('titulo').value,
+      descricao: document.getElementById('descricao').value,
+      data: document.getElementById('data').value,
+      local: document.getElementById('local').value,
+      contato: document.getElementById('contato').value
+    };
+
   });
 }
