2e1cebd154b4f6b18f13fbaa146a2e070a2210bd
Isaque Bersalim <isaquebersalim@gmail.com>
2025-06-10 21:13:00 -0300

    Detecta formulário de cadastro e adiciona escutador de evento 'submit'

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Verifica se está na página de cadastro
+const form = document.getElementById('form-cadastro');
+if (form) {
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+  });
+}
