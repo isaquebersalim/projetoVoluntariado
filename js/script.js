6f7c6f6741fde6f841aeb0b0cfe395f0e8683e7b
Isaque Bersalim <isaquebersalim@gmail.com>
2025-06-17 19:44:18 -0300

    integrando consumo da API ViaCEP para preenchimento automático de endereço.

--- js/script.js
+++ js/script.js
@@ -50,3 +50,32 @@ if (listaDiv) {
 
   }
 }
+
+const cepInput = document.getElementById('cep');
+
+if (cepInput) {
+  cepInput.addEventListener('blur', function() {
+    const cep = cepInput.value.replace(/\D/g, '');
+
+    if (cep.length !== 8) {
+      alert('CEP inválido! Digite um CEP com 8 dígitos.');
+      return;
+    }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.erro) {
+          alert('CEP não encontrado!');
+        } else {
+          document.getElementById('rua').value = data.logradouro;
+          document.getElementById('bairro').value = data.bairro;
+          document.getElementById('cidade').value = data.localidade;
+          document.getElementById('estado').value = data.uf;
+        }
+      })
+      .catch(() => {
+        alert('Erro ao buscar o CEP!');
+      });
+  });
+}
